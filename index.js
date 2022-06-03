81af47e735a356fc05a91117835cfe133e6918a5
Rokas Baltutis <rokasbalt02@gmail.com>
2022-06-03 16:34:22 +0300

    edit

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 const express = require('express');
 const PORT = process.env.PORT || '3000';
+var cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
+
+
+
+// use it before all route definitions
+app.use(cors({ origin: '*' }));
+
+// Add headers
+app.use(function(req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 app.get('/', (req, res) => {
     res.send("API with the whole purpose of it to just ommit letters from sentences if you dont like them.");
@@ -15,13 +41,15 @@ app.post('/', (req, res) => {
         sentance: req.body.sentance
     };
 
-    res.send(LetterRemover(text.sentance, text.letter));
+    res.send({
+        result: `string text ${LetterRemover(text.sentance,text.letter)} string text`
+    });
 });
-
+var x = "zzz";
 // PORT
 app.listen(PORT, () => console.log('Listening on port 3000..'));
 
 
 function LetterRemover(txt, char) {
-    return txt.replaceAll(char, '');;
+    return txt //txt.replaceAll(char, '');;
 }
